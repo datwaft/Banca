692d8936d3421c918b22bb22611e37e06aaa3ad8
datwaft <datwaftx@gmail.com>
2020-03-06 22:29:53 -0600

    database insert

--- sql/init.sql
+++ sql/init.sql
@@ -1,1 +1,18 @@
-insert into `user` (`username`, `password`) values ('system', 'admin');
+insert into `user` (`id`, `password`, `name`, `cellphone`, `cashier`, `client`) values
+('user1', 'password', 'USER1', '11111111', 0, 1),
+('user2', 'password', 'USER2', '22222222', 0, 1),
+('dual', 'password', 'DUAL', '33333333', 1, 1),
+('admin', 'password', 'ADMIN', '44444444', 1, 0);
+
+insert into `currency` (`code`, `name`, `conversion`, `interest_rate`) values
+('USD','Dólares', 1.0000, 0.30),
+('CRC', 'Colones', 570.0790, 0.44),
+('EUR', 'Euro', 0.8859, 0.20);
+
+insert into `account` (`owner`, `currency`, `dailylimit`) values
+('user1', 'EUR', 10000),
+('user1', 'USD', 10000),
+('user2', 'CRC', 2000000),
+('user2', 'USD', 10000),
+('dual', 'EUR', 20000),
+('dual', 'CRC', 2000000);
